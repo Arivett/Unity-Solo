05ea9e4cc8ddde57999d92be2944b782a19d1ffc
Arivett <arivettnunezmata@gmail.com>
2026-09-11 13:43:40 -0700

    Save movement

--- Nunez_2D_FOOD/Assets/Scripts/PlayerController.cs
+++ Nunez_2D_FOOD/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,21 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
+    public Vector2 moveInput = Vector2.zero;
+
+    PlayerInput input;
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      input = GetComponent<PlayerInput>();
+      rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
